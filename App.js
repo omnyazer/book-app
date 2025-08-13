0799b7e6eacbba36d125ad6835ed4b1a269c5bf0
omnya zerghouni <omnyazer@gmail.com>
2025-08-13 23:50:22 +0200

    tabs + 4 screens

--- App.js
+++ App.js
@@ -1,41 +1,55 @@
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+// Surfaces
 import Login from './src/surfaces/Login';
 import Feed from './src/surfaces/Feed';
+import Conversations from './src/surfaces/Conversations';
+import AddPost from './src/surfaces/AddPost';
+import Favorites from './src/surfaces/Favorites';
+import Profile from './src/surfaces/Profile';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+// Le Home = les 5 onglets en bas (comme sur la slide)
 function Home() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Conversations" component={Conversations} />
+      <Tab.Screen name="AddPost" component={AddPost} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 }
 
 export default function App() {
-  const [userLoggedIn, setIsUserLoggedIn] = useState(true);
+  // Le prof laisse souvent un useState(true) au début
+  const [userLoggedIn] = useState(true);
 
   return (
-    <NavigationContainer>
-      {userLoggedIn ? (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <StatusBar style="auto" />
         <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ headerShown: false }} 
-          />
+          {userLoggedIn ? (
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
+          ) : (
+            <Stack.Screen name="Login" component={Login} />
+          )}
         </Stack.Navigator>
-      ) : (
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-        </Stack.Navigator>
-      )}
-    </NavigationContainer>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
---
+++ src/surfaces/Favorites.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Favorites() {
+  return (
+    <View>
+      <Text>this will be the Favorites screen</Text>
+    </View>
+  );
+}
